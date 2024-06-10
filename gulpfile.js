0980e09ab29f1a827e26e719d05b3f904d6b5ed8
Jake Deery <jake.deery@ptfs-europe.com>
2024-06-10 16:08:36 +0100

    Added custom.less

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,37 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const less = require('gulp-less');
 
 
 // build sass
 function compileSass() {
   return gulp.src('sass/**/*.scss') // Path to your SCSS files
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on('error', console.error))
     .pipe(gulp.dest('css')); // Output directory for CSS files
 }
 gulp.task('compile-sass', compileSass);
 gulp.task('sass', compileSass);
 
 
+// build less
+function compileLess() {
+  return gulp.src('less/**/*.less') // Path to your LESS files
+    .pipe(less().on('error', console.error))
+    .pipe(gulp.dest('css')); // Output directory for CSS files
+}
+gulp.task('compile-less', compileLess);
+gulp.task('less', compileLess);
+
+
 // watch sass
 function watchSass() {
-    gulp.watch('scss/**/*.scss', compileSass);
+    gulp.watch('sass/**/*.scss', compileSass);
 }
 gulp.task('sass-watch', watchSass);
+
+
+// watch less
+function watchLess() {
+    gulp.watch('less/**/*.less', compileLess);
+}
+gulp.task('less-watch', watchLess);
--- theme.config.php
+++ theme.config.php
@@ -1,5 +1,4 @@
 <?php
 return [
     'extends' => 'bootstrap3',
-    'header-nav' => parse_ini_file('header-nav.ini', true),
 ];
