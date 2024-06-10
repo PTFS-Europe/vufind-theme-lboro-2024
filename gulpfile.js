5ad7be738401de8ee3e69812f4c93dc48887e83c
Jake Deery <jake.deery@ptfs-europe.com>
2024-06-10 17:49:39 +0100

    Hopefully ready for use

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,19 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const less = require('gulp-less');
-
-
-// build sass
-function compileSass() {
-  return gulp.src('sass/**/*.scss') // Path to your SCSS files
-    .pipe(sass().on('error', console.error))
-    .pipe(gulp.dest('css')); // Output directory for CSS files
-}
-gulp.task('compile-sass', compileSass);
-gulp.task('sass', compileSass);
+const gulp               = require('gulp');
+const less               = require('gulp-less');
+const lessPluginCleanCss = require('less-plugin-clean-css');
+const cleanCSSPlugin     = new lessPluginCleanCss({advanced: true});
 
 
 // build less
 function compileLess() {
   return gulp.src('less/**/*.less') // Path to your LESS files
-    .pipe(less().on('error', console.error))
+    .pipe(less({
+      plugins: [cleanCSSPlugin]
+    }).on('error', console.error))
     .pipe(gulp.dest('css')); // Output directory for CSS files
 }
 gulp.task('compile-less', compileLess);
 gulp.task('less', compileLess);
-
-
-// watch sass
-function watchSass() {
-    gulp.watch('sass/**/*.scss', compileSass);
-}
-gulp.task('sass-watch', watchSass);
 
 
 // watch less
