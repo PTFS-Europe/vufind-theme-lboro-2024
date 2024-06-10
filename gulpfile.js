71696166d3c2cf2b5d2c14c9c4bc03e4bfee8181
Jake Deery <jake.deery@ptfs-europe.com>
2024-06-10 15:13:15 +0100

    Added watch mode

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+
+// build sass
 function compileSass() {
   return gulp.src('sass/**/*.scss') // Path to your SCSS files
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('css')); // Output directory for CSS files
 }
+gulp.task('compile-sass', compileSass);
 gulp.task('sass', compileSass);
+
+
+// watch sass
+function watchSass() {
+    gulp.watch('scss/**/*.scss', compileSass);
+}
+gulp.task('sass-watch', watchSass);
