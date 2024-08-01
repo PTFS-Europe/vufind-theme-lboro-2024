1cface5848c419d2b90c1f898984df090acdb007
PerplexedTheta <jahdobble@llownd.net>
2024-08-01 10:06:43 +0100

    Added public files

---
+++ public/Databases/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: /Search/Results?type=AllFields&filter[]=format:\"Database\"&sort=title", true, 302);
+die();
+?>
---
+++ public/MyAccount/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: /MyResearch/UserLogin", true, 302);
+die();
+?>
