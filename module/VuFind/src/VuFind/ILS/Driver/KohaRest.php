98f87931df491c1b52095a1ecc7b78d593526a13
PerplexedTheta <jahdobble@llownd.net>
2024-08-01 15:59:47 +0100

    Added library_loc to KohaRest driver

--- module/VuFind/src/VuFind/ILS/Driver/KohaRest.php
+++ module/VuFind/src/VuFind/ILS/Driver/KohaRest.php
@@ -2016,7 +2016,12 @@ class KohaRest extends \VuFind\ILS\Driver\AbstractBase implements
             $entry = [
                 'id' => $id,
                 'item_id' => $item['item_id'],
-                'location' => $this->getItemLocationName($item),
+                'location' => (
+                    null == $item['location_description'] ?
+                    $this->getItemLocationName($item) :
+                    $this->getItemLocationName($item) + '-' + $item['location_description']
+                ),
+                'library_loc'  => $this->getItemLocationName($item),
                 'shelving_loc' => $item['location_description'],
                 'availability' => $available,
                 'status' => $status,
